3ddb5a998c193b3f983a3d4e3d2e34aaefeb1a4f
Henry Li <henry1943@163.com>
2026-06-29 15:59:36 +0800

    chore: update ESLint configuration to ignore build files and refine component paths in desktop main view

--- packages/desktop/src/mainview/main.tsx
+++ packages/desktop/src/mainview/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { App } from "../app";
 import "@/lib/electrobun";
+
+import { App } from "../app";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
